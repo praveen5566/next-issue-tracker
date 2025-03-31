840e58b06fcb9d30895f427675f1089646e407ec
Praveen Kumar Ningappa <12479379+praveen5566@users.noreply.github.com>
2025-03-31 16:10:27 -0700

    add nav link high lighting

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import { GiArtificialIntelligence } from "react-icons/gi";
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames';
 
 
 const NavBar = () => {
+
+  const currentPath = usePathname()
   const links = [
     {
       label: 'Dashboard', href: '/'
@@ -12,11 +18,22 @@ const NavBar = () => {
       label: 'Issues', href: '/issues'
     },
   ]
+  console.log(currentPath)
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/"><GiArtificialIntelligence /></Link>
       <ul className="flex space-x-6">
-        {links.map(link => <Link key={link.href} href={link.href} className="text-slate-500 hover:text-slate-800 transition-colors">{link.label}</Link>)}
+        {links.map(link =>
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classNames({
+              "text-slate-900": link.href === currentPath,
+              "text-slate-500": link.href !== currentPath,
+              "hover:text-slate-800 transition-colors": true
+            })}>
+            {link.label}
+          </Link>)}
       </ul>
     </nav>
   )
