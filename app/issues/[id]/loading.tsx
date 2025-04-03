120d0da4408011d1201867eec50fdfad42642238
praveen5566 <12479379+praveen5566@users.noreply.github.com>
2025-04-03 13:41:52 -0700

    add issue details page

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssueDetailsLoading = () => {
+  return (
+    <div>IssueDetailsLoading...</div>
+  )
+}
+
+export default IssueDetailsLoading
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewIssueLoading = () => {
+  return (
+    <div>NewIssueLoading...</div>
+  )
+}
+
+export default NewIssueLoading
