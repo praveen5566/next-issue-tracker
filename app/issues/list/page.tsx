7aa89de55449ea84f3de2bba0274881dc3c5366f
praveen5566 <12479379+praveen5566@users.noreply.github.com>
2025-04-11 13:30:28 -0700

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,21 @@ import { IssueStatusBadge, Link } from "@/app/components/index";
 import { Table } from "@radix-ui/themes";
 import { prisma } from "@/prisma/client";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const { status } = await searchParams;
+  const statuses = Object.values(Status);
+  const queryStatus = statuses.includes(status) ? status : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: queryStatus,
+    },
+  });
 
   return (
     <div>
